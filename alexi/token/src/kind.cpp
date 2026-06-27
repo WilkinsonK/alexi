9a9905aae873b21a4a5f2af55e335d2ae1591dbe
Keenan W. Wilkinson <keenanwilkinson@outlook.com>
2026-06-27 15:22:44 -0500

    Implement `Kind::validate`.

--- alexi/token/src/kind.cpp
+++ alexi/token/src/kind.cpp
@@ -1,8 +1,26 @@
+#include <format>
+
 #include "alexi/kind.hpp"
 
 namespace alexi::kind {
     std::shared_ptr<Self> Self::to_shared(void) const {
         return std::make_shared<Self>(*this);
+    }
+
+    Self Self::validate(void) const {
+        if (name.size() < 1)
+            throw "Kind defined with no identifier";
+        if (pattern.size() < 1)
+            throw std::format("Kind {} defined with no pattern", name);
+        if (pattern == R"(.*)")
+            throw std::format("Kind {} defined invalid pattern '{}'", name, pattern);
+        if (action == Action::NOTHING)
+            throw std::format("Kind {} defined with no action", name);
+        if ((action & Action::CONSUME) != Action::NOTHING && (action & Action::IGNORE) != Action::NOTHING)
+            throw std::format("Kind {} defined both to CONSUME and IGNORE its tokens", name);
+        if (order < 0 || order > 1)
+            throw std::format("Kind {} defined with an out-of-bounds order {}", name, order);
+        return *this;
     }
 
     bool Self::operator==(const Self &other) const {
