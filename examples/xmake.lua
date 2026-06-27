b23c3bcfcd699cce17a7ccb8b1f050479688b954
Keenan W. Wilkinson <keenanwilkinson@outlook.com>
2026-06-27 16:55:43 -0500

    Update build chain - examples only build when specified - switch mingw to use llvm-mingw - update Zed tasks to use specified options

--- examples/xmake.lua
+++ examples/xmake.lua
@@ -1,12 +1,12 @@
-set_default(false)
+if has_config("examples") then
+    target("basic_parsing")
+        set_kind("binary")
+        add_files("basic_parsing/main.cpp")
+        add_deps("alexi")
+        add_defines("LANG_FILE=\"$(projectdir)/examples/basic_parsing/lang.alexi\"")
 
-target("basic_parsing")
-    set_kind("binary")
-    add_files("basic_parsing/main.cpp")
-    add_deps("alexi")
-    add_defines("LANG_FILE=\"$(projectdir)/examples/basic_parsing/lang.alexi\"")
-
-target("basic_usage")
-    set_kind("binary")
-    add_files("basic_usage/main.cpp")
-    add_deps("alexi")
+    target("basic_usage")
+        set_kind("binary")
+        add_files("basic_usage/main.cpp")
+        add_deps("alexi")
+end
--- xmake.lua
+++ xmake.lua
@@ -9,6 +9,15 @@ if is_host("macosx") then
     set_config("runtimes", "c++_shared")
 end
 
+if is_plat("windows") then
+    add_cxflags("-std=c++23", {force = true})
+end
+
+option("examples")
+    set_default(false)
+    set_showmenu(true)
+    set_description("Builds project examples")
+
 option("testing")
     set_default(false)
     set_showmenu(true)
