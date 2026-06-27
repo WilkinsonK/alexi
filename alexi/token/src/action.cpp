6e1010d7e61bc6a6f690e03e9891eb302057ec70
Keenan W. Wilkinson <keenanwilkinson@outlook.com>
2026-06-27 15:22:23 -0500

    Update action.cpp - Actions when pushed to a stream are now concatentated as multi-flag   support.

--- alexi/token/src/action.cpp
+++ alexi/token/src/action.cpp
@@ -17,9 +17,9 @@ namespace alexi::action {
         return lhs | rhs;
     }
 
-    std::ostream &operator<<(std::ostream &os, const Self &Self) {
-        #define __strcase(T) case Self::T: os << #T; break
-        switch (Self) {
+    inline constexpr void ostream_helper(std::ostream &os, const Self &self) {
+        #define __strcase(T) case Self::T: os << "["#T"]"; break
+        switch (self) {
             __strcase(NOTHING);
             __strcase(CONSUME);
             __strcase(IGNORE);
@@ -27,7 +27,19 @@ namespace alexi::action {
             __strcase(UEOF);
             __strcase(UNKNOWN);
         };
-        return os;
         #undef __strcase
+    }
+
+    inline constexpr void ostream_helper(std::ostream &os, const Self &self, const Self &against) {
+        if ((self & against) == against) ostream_helper(os, self & against);
+    }
+
+    std::ostream &operator<<(std::ostream &os, const Self &self) {
+        ostream_helper(os, self, Action::CONSUME);
+        ostream_helper(os, self, Action::IGNORE);
+        ostream_helper(os, self, Action::MULTILINE);
+        ostream_helper(os, self, Action::UEOF);
+        ostream_helper(os, self, Action::UNKNOWN);
+        return os;
     }
 }
