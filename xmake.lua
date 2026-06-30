a8341ea3dbf4c6d8bdb8819ebe1a542a4c55d6bf
Keenan W. Wilkinson <keenanwilkinson@outlook.com>
2026-06-30 14:38:07 -0500

    Update build files

--- xmake.lua
+++ xmake.lua
@@ -2,13 +2,6 @@ set_languages("c++23", "c23")
 add_rules("plugin.compile_commands.autoupdate")
 add_rules("mode.debug", "mode.release")
 
--- Ensures that the toolchain and runtimes are compatible
--- with C++ modules.
-if is_host("macosx") then
-    set_config("toolchain", "clang")
-    set_config("runtimes", "c++_shared")
-end
-
 if is_plat("windows") then
     add_cxflags("-std=c++23", {force = true})
 end
